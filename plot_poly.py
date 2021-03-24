0b7e78fabaae83f77423bb4b9d5e3a6d9c43a528
tristanguerand <tristan@guerand.net>
2021-03-24 20:02:24 +0100

    add comments

--- plot_poly.py
+++ plot_poly.py
@@ -20,7 +20,19 @@ from PIL import ImageDraw
 from loader import Loader
 
 def plot_poly(df, poly_idx, data_path='dataset', out_path='out.png'):
+    """Plot a single polygon of an image
     
+    Args
+    -----
+    df: pd.Dataframe
+        the data dataframe
+        Cols : ['ImageId', 'geom', 'ClassType', 'Xmax', 'Ymin']
+    poly_idx: int
+        the idx of the row of the poly in df
+    data_path: str
+        path where the dataset is stored
+    out_path: str
+        path where the output image is saved"""
     po = ast.literal_eval(df['geom'].iloc[poly_idx])
     
     
@@ -39,10 +51,24 @@ def plot_poly(df, poly_idx, data_path='dataset', out_path='out.png'):
     img3.save(out_path)
     
 def plot_polys(df, class_type, img_id, data_path='dataset', out_path='out.png'):
-
+    """Plot all the polygons of a single image of a single class
+    
+    Args
+    -----
+    df: pd.Dataframe
+        the data dataframe
+        Cols : ['ImageId', 'geom', 'ClassType', 'Xmax', 'Ymin']
+    class_type: int
+        the number id of the class to show
+    img_id: str
+        the id of the image
+    data_path: str
+        path where the dataset is stored
+    out_path: str
+        path where the output image is saved"""
     polys = []
     
-    for pol in df['geom'][(df['ImageId']==img_id) & (df['ClassType']==class_type)].values:
+    for pol in df['geom'][(df['ImageId']==img_id) & (df['ClassType']==class_type)].valuesgi:
         polys.append(ast.literal_eval(pol))
     
     img = Image.open(os.path.join(data_path, img_id + '.tif'))
