8d86a4b423bcd3f72401cb455e2d4821c042bb7b
tristanguerand <tristan@guerand.net>
2021-03-29 10:24:56 +0200

    tiff to jpg

--- viz/crop.py
+++ viz/crop.py
@@ -8,10 +8,13 @@ Created on Sat Mar 27 14:58:58 2021
 import tifffile as tiff
 import matplotlib.pyplot as plt
 import cv2
+import numpy as np
 
 def crop(tiff_img, ratio, out_path):
     """
     Crop a tiff image with a certain ration to reduce its size
+    
+    https://stackoverflow.com/questions/13714454/specifying-and-saving-a-figure-with-exact-size-in-pixels
     
     Args
     -----
@@ -24,17 +27,26 @@ def crop(tiff_img, ratio, out_path):
         the pave to save the cropped img
     """
     
-    
-    
     image = tiff.imread(tiff_img) 
+    h, w, c = image.shape
+    plt.figure(figsize=(h/1000, w/1000), dpi=100)
+    #image = tiff.imread(tiff_img) 
     tiff.imshow(image)
     
     ax = plt.gca()    
     ax.set_axis_off()
     
-    plt.savefig(out_path, bbox_inches='tight',pad_inches = 0)
+    plt.savefig(out_path, bbox_inches='tight',pad_inches = 0, dpi=500)
     
     plt.show()
+    # image = np.moveaxis(image, 0, -1)
+    # fig = plt.figure(frameon=False)
+    # fig.set_size_inches(w,h)
+    # ax = plt.Axes(fig, [0., 0., 1., 1.])
+    # ax.set_axis_off()
+    # fig.add_axes(ax)
+    # ax.imshow(image)
+    # fig.savefig('figure.png', dpi=1)
     
 path = r'C:\Users\trist\Documents\CS\3A\GRM\GRM_GraphCut\GRM_GraphCut\dataset\6010_1_2.tif'
 ratio = 0.75
